2bccc963358a5fd752a9b126f35efceb97af116b
YoungBeom Kim <darania@Kookmin.ac.kr>
2021-02-17 09:47:23 +0900

    first_commit

--- func.c
+++ func.c
@@ -1,1 +1,181 @@
-sdfasdfadsf
+#include "header.h"
+
+
+unsigned char getHex(unsigned char ch)
+{
+	unsigned char hex = 0;
+
+	if (ch >= '0' && ch <= '9')
+	{
+		hex = ch - '0';
+	}
+
+	else if (ch >= 'a' && ch <= 'f')
+	{
+		hex = ch - 'a' + 10;
+	}
+
+	else if (ch >= 'A' && ch <= 'F')
+	{
+		hex = ch - 'A' + 10;
+	}
+	return hex;
+}
+
+void convertStr2Byte(unsigned char* from, int size, unsigned char* to)
+{
+	int cnt_i = 0;
+	int cnt_j = 0;
+	int ch = 0;
+
+	for (cnt_i = 0; cnt_i < size; cnt_i += 2, cnt_j++)
+	{
+
+		ch = from[cnt_i];
+		to[cnt_j] = getHex(ch);
+		to[cnt_j] <<= 4;
+		ch = from[cnt_i + 1];
+		to[cnt_j] |= getHex(ch);
+	}
+
+}
+
+
+void InvShiftRow(unsigned char* state)
+{
+	int temp, temp2;
+	temp = state[13]; //1 bytes Rightshift
+	state[13] = state[9];
+	state[9] = state[5];
+	state[5] = state[1];
+	state[1] = temp;
+
+	temp = state[10]; //2 bytes Rightshift
+	temp2 = state[14];
+	state[10] = state[2];
+	state[14] = state[6];
+	state[2] = temp;
+	state[6] = temp2;
+
+	temp = state[3]; //3 bytes Rightshift
+	state[3] = state[7];
+	state[7] = state[11];
+	state[11] = state[15];
+	state[15] = temp;
+}
+void InvSubByte(unsigned char* state)
+{
+	int cnt_i;
+	for (cnt_i = 0; cnt_i < 16; cnt_i++)
+	{
+		*(state + cnt_i) = rsbox[state[cnt_i]];
+	}
+}
+
+void AddRoundKey_1Round(unsigned char* state, unsigned char* key)
+{
+	int cnt_i;
+	for (cnt_i = 0; cnt_i < AES_PLANETXT_LEN; cnt_i++)
+	{
+		state[cnt_i] ^= key[cnt_i];
+	}
+
+}
+
+byte Find_HammingDistance(unsigned char before_distance, unsigned char after_distance)
+{
+	byte temp = before_distance ^ after_distance;
+	byte count = 0;
+	int cnt_i = 0;
+
+	//printf("temp = %02X\n", temp);
+	for (cnt_i = 0; cnt_i < 8; cnt_i++)
+	{
+		if (((temp >> cnt_i) & 0x01) == 1)
+		{
+			count++;
+		}
+	}
+
+	//printf("%x\n", count);
+	return count;
+}
+
+void Calculates_SumX(double* Sum_xx, double* Sum_Ex, double TraceTemp[TRACE_NUM][TRACE_LENGTH])
+{
+	/*
+	 *double Sum_xx[TRACE_LENGTH] = { 0x00, };
+	 *double Sum_Ex[TRACE_LENGTH] = { 0x00, };
+	 *float TraceTemp[TRACE_NUM][TRACE_LENGTH] = { 0x00, };
+	*/
+
+	int cnt_i = 0, cnt_j = 0, cnt_k = 0;
+	double sum_xx = 0x00;
+	double sum_ex = 0x00;
+
+	for (cnt_i = 0; cnt_i < TRACE_LENGTH; cnt_i++)
+	{
+		for (cnt_j = 0; cnt_j < TRACE_NUM; cnt_j++)
+		{
+			sum_ex += TraceTemp[cnt_j][cnt_i];
+			sum_xx += (TraceTemp[cnt_j][cnt_i] * TraceTemp[cnt_j][cnt_i]);
+		}
+		Sum_Ex[cnt_i] = sum_ex;
+		Sum_xx[cnt_i] = sum_xx;
+		sum_ex = 0x00;
+		sum_xx = 0x00;
+	}
+}
+
+void Calculates_SumY(float Sum_yy[S_BOX][GUESSKEY], float Sum_Ey[S_BOX][GUESSKEY], byte HammingDistance[S_BOX][TRACE_NUM][GUESSKEY])
+{
+	/*
+	 *float Sum_yy[S_BOX][GUESSKEY] = { 0x00, };
+	 *float Sum_Ey[S_BOX][GUESSKEY] = { 0x00, };
+	 *byte HammingDistance[AES_PLANETXT_LEN][TRACE_NUM][GUESSKEY] = {0x00,};
+	 */
+
+	int cnt_i = 0, cnt_j = 0, cnt_k = 0;
+	float sum_yy = 0x00;
+	float sum_ey = 0x00;
+
+	for (cnt_i = 0; cnt_i < S_BOX; cnt_i++)
+	{
+		for (cnt_j = 0; cnt_j < GUESSKEY; cnt_j++)
+		{
+			for (cnt_k = 0; cnt_k < TRACE_NUM; cnt_k++)
+			{
+				sum_ey += HammingDistance[cnt_i][cnt_k][cnt_j];
+				sum_yy += (HammingDistance[cnt_i][cnt_k][cnt_j] * HammingDistance[cnt_i][cnt_k][cnt_j]);
+			}
+			Sum_Ey[cnt_i][cnt_j] = sum_ey;
+			Sum_yy[cnt_i][cnt_j] = sum_yy;
+			sum_ey = 0x00;
+			sum_yy = 0x00;
+		}
+
+	}
+}
+
+void Calculates_SumXY(double Sum_xy[S_BOX][GUESSKEY][TRACE_LENGTH], double TraceTemp[TRACE_NUM][TRACE_LENGTH], byte HammingDistance[S_BOX][TRACE_NUM][GUESSKEY])
+{
+	int cnt_i = 0, cnt_j = 0, cnt_k = 0, cnt_s = 0;
+	double sum_xy = 0x00;
+
+	for (cnt_i = 0; cnt_i < S_BOX; cnt_i++)
+	{
+		for (cnt_j = 0; cnt_j < GUESSKEY; cnt_j++)
+		{
+			for (cnt_k = 0; cnt_k < TRACE_LENGTH; cnt_k++)
+			{
+				for (cnt_s = 0; cnt_s < TRACE_NUM; cnt_s++)
+				{
+					sum_xy += (double)(HammingDistance[cnt_i][cnt_s][cnt_j] * TraceTemp[cnt_s][cnt_k]);
+				}
+				Sum_xy[cnt_i][cnt_j][cnt_k] = sum_xy;
+				sum_xy = 0x00;
+			}
+		}
+
+	}
+}
---
+++ main.c
@@ -0,0 +1,181 @@
+#include "header.h"
+
+FILE* FILE_Trace = NULL;
+FILE* FILE_PT = NULL;
+FILE* CPA_peak = NULL;
+
+byte bytes_value = 0;
+byte ciphertext[TRACE_NUM][AES_PLANETXT_LEN] = { 0x00, };
+byte buffer[64] = { 0, };
+byte HammingDistance[AES_PLANETXT_LEN][TRACE_NUM][GUESSKEY];
+byte guess_key[16] = { 0x00, };
+char str[256] = { 0, };
+
+float Corr[S_BOX][TRACE_LENGTH][GUESSKEY] = { 0x00, };
+double TraceTemp[TRACE_NUM][TRACE_LENGTH] = { 0x00, };
+double Sum_xy[S_BOX][GUESSKEY][TRACE_LENGTH] = { 0x00, };
+double Sum_xx[TRACE_LENGTH] = { 0x00, };
+double Sum_Ex[TRACE_LENGTH] = { 0x00, };
+float Sum_yy[S_BOX][GUESSKEY] = { 0x00, };
+float Sum_Ey[S_BOX][GUESSKEY] = { 0x00, };
+
+double co_co[TRACE_LENGTH];
+double MAX_peak[GUESSKEY];
+double temp;
+
+
+int main()
+{
+	int cnt_i = 0, cnt_j = 0, cnt_k = 0, cnt_s = 0;
+	FILE_PT = fopen("C:\\Users\\YoungBeom Kim\\source\\repos\\AES_HW_trace\\ciphertext.txt", "r");
+	assert(FILE_PT != NULL);
+
+	//[Read CipherText]**************************************************************************************************************************
+	for (cnt_i = 0; cnt_i < TRACE_NUM; cnt_i++)
+	{
+		fgets(buffer, sizeof(buffer), FILE_PT); //! 한줄씩만 읽는함수
+		convertStr2Byte(buffer, 32, ciphertext[cnt_i]);
+
+		/*for (int cnt_j = 0; cnt_j < AES_PLANETXT_LEN; cnt_j++)
+		{
+			printf("%02X ", ciphertext[cnt_i][cnt_j]);
+		}
+		printf("\n");*/
+
+	}
+	fclose(FILE_PT);
+
+
+	//[Read Trace]*******************************************************************************************************************************
+	for (cnt_i = 0; cnt_i < TRACE_NUM; cnt_i++)
+	{
+		sprintf(str, "C:\\Users\\YoungBeom Kim\\source\\repos\\AES_HW_trace\\trace_%0.5d.txt", cnt_i);
+		FILE_Trace = fopen(str, "rb");
+		assert(FILE_Trace != NULL);
+		for (cnt_j = 0; cnt_j < TRACE_LENGTH; cnt_j++)
+		{
+			fscanf(FILE_Trace, "%lf", &TraceTemp[cnt_i][cnt_j]);
+			//printf("%lf\n", TraceTemp[cnt_i][cnt_j]);
+		}
+		//printf("\n");
+		//return 0;
+		fclose(FILE_Trace);
+	}
+
+
+
+	//[Caluates HammingDistance]*******************************************************************************************************************************
+	for (cnt_i = 0; cnt_i < S_BOX; cnt_i++)
+	{
+		//printf("\n*******[S_BOX %02X]*******\n", cnt_i);
+		for (cnt_j = 0; cnt_j < GUESSKEY; cnt_j++)
+		{
+			guess_key[cnt_i] = cnt_j;
+			//printf("\n\n*******[GUESS KEY %02X]*******\n", cnt_j);
+			for (cnt_k = 0; cnt_k < TRACE_NUM; cnt_k++)
+			{
+				byte before_distance = ciphertext[cnt_k][cnt_i];
+				bytes_value = ciphertext[cnt_k][cnt_i];
+				bytes_value = rsbox[bytes_value ^ guess_key[cnt_i]];
+				byte after_distance = bytes_value;
+				HammingDistance[cnt_i][cnt_k][cnt_j] = Find_HammingDistance(before_distance, after_distance);
+				//printf("%d ", HammingDistance[cnt_i][cnt_k][cnt_j]);
+			}
+		}
+	}
+
+	//[Caluates SumEx, xx, Ey, yy]*******************************************************************************************************************************
+	Calculates_SumX(Sum_xx, Sum_Ex, TraceTemp);
+	Calculates_SumY(Sum_yy, Sum_Ey, HammingDistance);
+	Calculates_SumXY(Sum_xy, TraceTemp, HammingDistance);
+
+	//for (cnt_i = 0; cnt_i < TRACE_LENGTH; cnt_i++)
+	//{
+	//	printf("%f\n", Sum_xx[cnt_i]);
+	//
+	//}
+
+	//for (cnt_i = 0; cnt_i < S_BOX; cnt_i++)
+	//{
+	//	printf("*****[SBOX %d]****\n", cnt_i);
+	//	for (cnt_j = 0; cnt_j < GUESSKEY; cnt_j++)
+	//	{
+	//		printf("%f\n", Sum_yy[cnt_i][cnt_j]);
+	//	}
+	//}
+
+	//for (cnt_i = 0; cnt_i < S_BOX; cnt_i++)
+	//{
+	//	printf("*****[SBOX %d]****\n", cnt_i);
+	//	for (cnt_j = 0; cnt_j < GUESSKEY; cnt_j++)
+	//	{
+	//		printf("%f\n", Sum_Ey[cnt_i][cnt_j]);
+	//	}
+	//}
+
+	/*for (cnt_i = 0; cnt_i < S_BOX; cnt_i++)
+	{
+		printf("*****[SBOX %d]****\n", cnt_i);
+		for (cnt_j = 0; cnt_j < GUESSKEY; cnt_j++)
+		{
+			printf("*****[GUESSKEY %d]****\n", cnt_j);
+			for (cnt_k = 0; cnt_k < TRACE_LENGTH; cnt_k++)
+			{
+				printf("%lf\n", Sum_xy[cnt_i][cnt_j][cnt_k]);
+			}
+		}
+	}*/
+
+
+	//[Caluates Correlation coefficient]*******************************************************************************************************************************
+
+	for (cnt_i = 0; cnt_i < S_BOX; cnt_i++)
+	{
+		//printf("\n*******[S_BOX %02X]*******\n", cnt_i);
+		for (cnt_j = 0; cnt_j < GUESSKEY; cnt_j++)
+		{
+			//printf("\n*******[GUESSKEY %02X]*******\n", cnt_j);
+			for (cnt_k = 0; cnt_k < TRACE_LENGTH; cnt_k++)
+			{
+				co_co[cnt_k] = ((TRACE_NUM)*Sum_xy[cnt_i][cnt_j][cnt_k] - (Sum_Ex[cnt_k] * Sum_Ey[cnt_i][cnt_j])) / ((sqrt((TRACE_NUM)*Sum_xx[cnt_k] - (Sum_Ex[cnt_k] * Sum_Ex[cnt_k]))) * (sqrt((double)(TRACE_NUM)*Sum_yy[cnt_i][cnt_j] - ((double)Sum_Ey[cnt_i][cnt_j] * (double)Sum_Ey[cnt_i][cnt_j]))));
+			}
+			temp = co_co[0];
+			for (cnt_s = 1; cnt_s < TRACE_LENGTH; cnt_s++)
+			{
+				if (temp < co_co[cnt_s])
+				{
+					temp = co_co[cnt_s];
+				}
+			}
+			MAX_peak[cnt_j] = temp;
+			//printf("MAXpeak : %lf\n", MAX_peak[cnt_j]);
+		}
+
+		temp = MAX_peak[0];
+		for (cnt_s = 0; cnt_s < GUESSKEY; cnt_s++)
+		{
+			if (temp <= MAX_peak[cnt_s])
+			{
+				temp = MAX_peak[cnt_s];
+				guess_key[cnt_i] = cnt_s;
+				//printf("%02X\n", guess_key[cnt_j]);
+			}
+		}
+
+		for (cnt_j = 0; cnt_j < TRACE_LENGTH; cnt_j++)
+		{
+			co_co[cnt_j] = ((TRACE_NUM)*Sum_xy[cnt_i][guess_key[cnt_i]][cnt_j] - (Sum_Ex[cnt_j] * Sum_Ey[cnt_i][guess_key[cnt_i]])) / ((sqrt((TRACE_NUM)*Sum_xx[cnt_j] - (Sum_Ex[cnt_j] * Sum_Ex[cnt_j]))) * (sqrt((double)(TRACE_NUM)*Sum_yy[cnt_i][guess_key[cnt_i]] - ((double)Sum_Ey[cnt_i][guess_key[cnt_i]] * (double)Sum_Ey[cnt_i][guess_key[cnt_i]]))));
+		}
+
+		sprintf(str, "C:\\Users\\YoungBeom Kim\\source\\repos\\AESHW_CPA210215\\CPA_Peak\\!CPA_%d_peak_%x.txt", cnt_i, guess_key[cnt_i]);
+		CPA_peak = fopen(str, "w");
+
+		for (cnt_j = 0; cnt_j < TRACE_LENGTH; cnt_j++)
+		{
+			fprintf(CPA_peak, "%lf\n", co_co[cnt_j]);
+		}
+		fclose(CPA_peak);
+	}
+
+	return 0;
+}
